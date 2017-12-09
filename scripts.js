da733a3515606aa10a24267a0f95fdc046883478
Ayellet Tal <tal.ayellet@gmail.com>
2017-12-09 09:47:54 +0200

    added random color ommitted implementation

--- scripts.js
+++ scripts.js
@@ -1,16 +1,30 @@
+fLoadQuote = function () {
+    $.ajax({
+        cache: false,
+        url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+        dataType: "json",
+        success: function(a) {
+            $("#quote-div").html("<i class='fa fa-quote-left' aria-hidden='true'></i> "
+                + a[0].content + "<br>"
+                + "<p style='font-size: 18px; text-align: right; display: block;'>— "
+                + a[0].title + "</p>")
+        }
+    });
+}
+
 fGetRandomColor = function () {
     var letters = "0123456789ABCDEF";
     var color = "#";
 
     for (var i = 0; i < 6; i++) {
-        color+=letters[Math.floor(Math.random() * 16)]
+        color+=letters[Math.floor(Math.random() * 16)];
     }
 
     return color;
 }
 
 $(document).ready(function() {
-    // fLoadQuote();
+    fLoadQuote();
     $("#new-quote-btn").on('click', fLoadQuote);
     $("#new-quote-btn").on("click", function () {
         var color = fGetRandomColor();
